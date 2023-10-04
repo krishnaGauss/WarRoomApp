10f6b0bb1a80aa04180b902c6d0e686e271e7f1e
Krishna Goswami <114379355+krishnaGauss@users.noreply.github.com>
2023-10-04 18:13:15 +0530

    Splash screen added

--- app/src/main/java/com/example/firebaseauth/Splashscreen.kt
+++ app/src/main/java/com/example/firebaseauth/Splashscreen.kt
@@ -1,11 +1,22 @@
 package com.example.firebaseauth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class Splashscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        val mDeelay: Long=3000
+        Handler(Looper.getMainLooper()).postDelayed({
+          startActivity((Intent(this, MainActivity::class.java)))
+        }, mDeelay)
+//        Handler(Looper.getMainLooper()).postDelayed(3000){
+//            startActivity(Intent(this, MainActivity::class.java))
+//        }
     }
 }
